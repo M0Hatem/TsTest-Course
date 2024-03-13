e7d15a7e0e993cba651bf02f243d795b9a1d3377
Mohamed <moh.medh.tem1@gmail.com>
2024-03-13 15:09:14 +0200

    RegisterRequest,LoginRequest tested

---
+++ Section 7 - Low mocks tests/src/test/server_app/LoginRequests.test.ts
@@ -0,0 +1,82 @@
+import {RequestTestWrapper} from "./test_utils/RequestTestWrapper";
+import {ResponseTestWrapper} from "./test_utils/ResponseTestWrapper";
+import {HTTP_CODES, HTTP_METHODS} from "../../app/model/ServerModel";
+import {UserCredentialsDataAccess} from "../../app/data/UserCredentialsDataAccess";
+import {Server} from "../../app/server/Server";
+import {DataBase} from "../../app/data/DataBase";
+
+
+
+jest.mock("../../app/data/DataBase")
+
+const requestWrapper = new RequestTestWrapper()
+const responseWrapper  = new ResponseTestWrapper()
+
+const server={
+    listen:()=>{},
+    close:()=>{}
+}
+jest.mock('http',()=>({
+    createServer:(cb:Function)=>{
+        cb(requestWrapper,responseWrapper)
+        return server
+    }
+}))
+
+const someCredentials ={
+   id:'',
+    userName:'someUserName',
+    password:'somePassword'
+}
+
+const someToken = '1234'
+
+const jsonHeader = { 'Content-Type': 'application/json' }
+describe('LoginRequests test suite', () => {
+    const insertSpy = jest.spyOn(DataBase.prototype, 'insert');
+    const getBySpy = jest.spyOn(DataBase.prototype, 'getBy');
+
+    beforeEach(()=>{
+        requestWrapper.headers['user-agent'] = 'jest tests'
+    })
+    afterEach(()=>{
+        requestWrapper.clearFields()
+        responseWrapper.clearFields()
+        jest.clearAllMocks();
+    })
+    it('should login the user and return token if ture credential passed',async () => {
+        requestWrapper.method = HTTP_METHODS.POST;
+        requestWrapper.body = someCredentials
+        requestWrapper.url = 'localHost:8080/login'
+
+        getBySpy.mockResolvedValueOnce(someCredentials);
+
+        insertSpy.mockResolvedValueOnce(someToken);
+        await new Server().startServer()
+
+        await new Promise(process.nextTick)
+
+        expect(responseWrapper.statusCode).toBe(HTTP_CODES.CREATED);
+        expect(responseWrapper.body).toEqual({
+            token: someToken
+        })
+        expect(responseWrapper.headers).toContainEqual(jsonHeader);
+    });
+    it('should not login the user if false credential passed',async () => {
+        requestWrapper.method = HTTP_METHODS.POST;
+        requestWrapper.body = {
+            userName:'someUserName',
+            password:'wrong Password'
+        }
+        requestWrapper.url = 'localHost:8080/login'
+
+        jest.spyOn(UserCredentialsDataAccess.prototype,'getUserByUserName').mockResolvedValueOnce({}as any)
+
+        await new Server().startServer()
+
+        await new Promise(process.nextTick)
+
+        expect(responseWrapper.statusCode).toBe(HTTP_CODES.NOT_fOUND)
+        expect(responseWrapper.body).toEqual('wrong username or password')
+    });
+});
