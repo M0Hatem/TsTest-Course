d290679272506f42a4f3b12f7fc953074152272d
Mohamed <moh.medh.tem1@gmail.com>
2024-03-14 13:38:08 +0200

    section Done!

--- Section 8 - Integration testing/jest.config.ts
+++ Section 8 - Integration testing/jest.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "@jest/types";
 
 const baseDir = "<rootDir>/src/app";
-const baseTestDir = "<rootDir>/src/test/server_app";
+const baseTestDir = "<rootDir>/src/test/IntegrationTests";
 
 const config: Config.InitialOptions = {
   preset: "ts-jest",
